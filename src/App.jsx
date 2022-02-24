dbf6fa6ef43c7980afee10c76ae2857d40ac7c43
Eric Moran <emoran0403@gmail.com>
2022-02-24 12:37:12 -0500

    added error catching for fetch failure, super close to displaying which movie a character appears in

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,51 @@
 import React from "react";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 //*Components
 import FilmsComp from "./components/FilmsComp";
 import PeopleComp from "./components/PeopleComp";
 
 const App = () => {
-  const [films, setFilms] = useState([]);
-  const [people, setPeople] = useState([]);
-  const [view, setView] = useState(0);
+  const [films, setFilms] = useState([]); // controls the films state
+  const [people, setPeople] = useState([]); // controls the people state
+  const [view, setView] = useState(0); // controls the view state
 
   const getFilms = async () => {
-    //this is view 1
-    // fetches data from the api
-    const response = await fetch("https://ghibliapi.herokuapp.com/films"); // this is the fetch
-    const filmData = await response.json(); // parses the response as JSON data to produce a JS object
-    // console.log(filmData); // logs the films object
-    setFilms(filmData); // passes the films object to the films state, which is then sent to the Films Component
-    setView(1);
+    try {
+      // this is view 1
+      // fetches data from the api
+      const response = await fetch("https://ghibliapi.herokuapp.com/films"); // this is the fetch
+      const filmData = await response.json(); // parses the response as JSON data to produce a JS object
+      console.log(filmData); // logs the films object
+      setFilms(filmData); // passes the films object to the films state, which is then sent to the Films Component
+      setView(1); // displays the Films Component if fetch is successful
+    } catch (error) {
+      console.log(error);
+      setView(3); // displays an error if fetch is unsuccessful
+    }
   };
 
   const getPeople = async () => {
-    //this is veiw 2
-    // fetches data from the api
-    const response1 = await fetch("https://ghibliapi.herokuapp.com/people"); // this is the fetch
-    const peopleData = await response1.json(); // parses the response as JSON data to produce a JS object
-    // console.log(peopleData); // logs the people object
-    setPeople(peopleData); // passes the people object to the people state, which is then sent to the People Component
-    setView(2);
-    //**********just in case user clicks on people first */
-    const response2 = await fetch("https://ghibliapi.herokuapp.com/films"); // this is the fetch
-    const filmData = await response2.json(); // parses the response as JSON data to produce a JS object
-    // console.log(filmData); // logs the films object
-    setFilms(filmData);
+    try {
+      // this is veiw 2
+      // fetches data from the api
+      const response1 = await fetch("https://ghibliapi.herokuapp.com/people"); // this is the fetch
+      const peopleData = await response1.json(); // parses the response as JSON data to produce a JS object
+      console.log(peopleData); // logs the people object
+      setPeople(peopleData); // passes the people object to the people state, which is then sent to the People Component
+      setView(2); // displays the People Component if fetch is successful
+
+      //**********just in case user clicks on people first */
+
+      const response2 = await fetch("https://ghibliapi.herokuapp.com/films"); // this is the fetch
+      const filmData = await response2.json(); // parses the response as JSON data to produce a JS object
+      // console.log(filmData); // logs the films object
+      setFilms(filmData);
+    } catch (error) {
+      console.log(error);
+      setView(3); // displays an error page if fetch is unsuccessful
+    }
   };
 
   return (
@@ -46,6 +59,15 @@ const App = () => {
           Gibblie People
         </button>
       </div>
+
+      {view === 0 && ( // this displays Studio Gobblie art as a default page
+        <>
+          <div className="d-flex mt-4 justify-content-center">
+            <img src="Studio_Ghiblei_Logo.jpg" alt="Studio Gibbles" width={1200} height="auto" />
+          </div>
+        </>
+      )}
+
       {view === 1 && ( // this displays Films Component when the films button is clicked
         <>
           <FilmsComp movies={films} />
@@ -55,6 +77,19 @@ const App = () => {
       {view === 2 && ( // this displays People Component when the films button is clicked
         <>
           <PeopleComp characters={people} films={films} />
+        </>
+      )}
+
+      {view === 3 && ( // this displays when fetch fails
+        <>
+          <div className="d-flex mt-4 justify-content-center">
+            <div>
+              <img src="sadgibbliee.jpg" alt="aww" width={608} height="auto" />
+            </div>
+          </div>
+          <div className="d-flex mt-4 justify-content-center">
+            <h1>Could not complete the request</h1>
+          </div>
         </>
       )}
     </>
--- src/components/PeopleComp.jsx
+++ src/components/PeopleComp.jsx
@@ -3,39 +3,63 @@ import React from "react";
 const PeopleComp = (props) => {
   // console.log(props.films);
 
-  // for (let i = 0; i <= props.characters.length; i++) {
+  // let temp = "https://ghibliapi.herokuapp.com/films/";
+  // console.log(temp.length);
+
+  // for (let i = 0; i <= props.characters.length - 1; i++) {
   //   // console.log(props.characters[i]);
-  //   for (let j = 0; j <= props.characters[i].films.length; j++) {
+  //   for (let j = 0; j <= props.characters[i].films.length - 1; j++) {
   //     console.log(props.characters[i].films[0]);
+  //     // console.log(`character ${i}`);
   //   }
   // }
 
   /**
-   * props.characters[i].films[0] this gets the first film in the film array, which is the appearance
-   * i can then do some string manip to match that to the film
-   * then finally display the film name
+   * props.characters[i].films[0] this gets the first film in the film array, which is the movie appearance
+   * since there is only 1 film in the array, i use position [0]
+   * the film id is at the end, and each url is the same length
+   * props.films[0].substring(38) gives me the id of the particular movie
+   * with the movie id from character, i then have to go thru the movies array, then into each movie object, and compare it with the id key
+   * if they are a match, then i need to get that movies title from the title property
+   *
+   *
+   *
    *
    * a similar process can be done for the species
    *
    *
    */
 
+  // const getMovieTitle = (character, films) => {
+  //   console.log(character);
+  //   console.log(films);
+
+  //   // let movieIDfromChar = character.films[0].substring(38); // this is the movie id from the character
+  //   // for (let i = 0; i <= films.length; i++) {
+  //   //   // this will go thru each film
+  //   //   let movieIDfromMovie = films[i].id; // this is the movie id, which we are looking to match to the movie id from character
+  //   //   if (movieIDfromChar === movieIDfromMovie) {
+  //   //     return films[i].title; // if it is a match, then return the title
+  //   //   }
+  //   // }
+  // };
+
   return (
     <>
       <div className="container">
         <div className="row justify-content-center mt-5">
-          {props.characters.map((props) => (
-            <div key={props.id} className="card col-md-6">
+          {props.characters.map((character) => (
+            <div key={character.id} className="card col-md-6">
               <div className="card-body">
                 <h5 className="card-title">
-                  {props.name} | {props.gender} | {props.age}
+                  {character.name} | {character.gender} | {character.age}
                 </h5>
                 <h6 className="card-subtitle mb-2 text-muted">
-                  Species: props.species | Eye-color: {props.eye_color} | Hair-Color: {props.hair_color}
+                  {/*Species: character.species | */}Eye-color: {character.eye_color} | Hair-Color: {character.hair_color}
                 </h6>
-                <p className="card-text">Appears in: props.films</p>
+                <p className="card-text">Appears in: {character.films[0].substring(38)}</p> {/* - getMovieTitle(character, props.films)shows the movie id*/}
                 <footer className="blockquote-footer">
-                  <a className="btn btn-success btn-sm" href={props.url} target="_blank">
+                  <a className="btn btn-success btn-sm" href={character.url} target="_blank">
                     View my JSON
                   </a>
                 </footer>
@@ -49,3 +73,11 @@ const PeopleComp = (props) => {
 };
 
 export default PeopleComp;
+
+/**
+ *
+ * string - slice if i know where the id starts and ends, and only if it starts at the same index every time = it probably does, but not the best option
+ * string.substring(x) will grab the end of the string automaticaly - a little easier than slice, but has the same drawbacks
+ * string.split('charToSplitAt') will put the id as an element into a new array
+ *
+ */
